1ee6c5e7fe3b53c60986f64fe3e9398596eaec6c
reshetovsn <reshetovsn@gmail.com>
2023-08-02 17:58:58 +0300

    added seed.sql and testPage

---
+++ app/src/main/resources/seed.sql
@@ -0,0 +1,4 @@
+INSERT INTO url (name, created_at)
+    VALUES ('https://youtube.com', '2023-03-01 14:03:40'),
+            ('https://ru.hexlet.io', '2023-03-01 14:03:59'),
+            ('https://gmail.com', '2023-03-01 14:04:04');
